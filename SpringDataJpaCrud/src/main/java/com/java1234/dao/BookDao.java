614ca30c552aafb8c33c2aeeb20141d5dc39d2ef
wujun728 <wujun728@hotmail.com>
2020-09-08 01:44:31 +0800

    add spirngboot demo

--- SpringDataJpaCrud/src/main/java/com/java1234/dao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.java1234.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java1234.entity.Book;
-
-public interface BookDao extends JpaRepository<Book, Integer>{
-
-}
