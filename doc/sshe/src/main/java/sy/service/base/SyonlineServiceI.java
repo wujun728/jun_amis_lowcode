b7d3d7d8c4e505d85495e196729bff06ebacd154
wujun728 <wujun728@hotmail.com>
2021-02-26 00:18:10 +0800

    1

---
+++ doc/sshe/src/main/java/sy/service/base/SyonlineServiceI.java
@@ -0,0 +1,13 @@
+package sy.service.base;
+
+import sy.model.base.Syonline;
+import sy.service.BaseServiceI;
+
+/**
+ * 
+ * @author 孙宇
+ * 
+ */
+public interface SyonlineServiceI extends BaseServiceI<Syonline> {
+
+}
