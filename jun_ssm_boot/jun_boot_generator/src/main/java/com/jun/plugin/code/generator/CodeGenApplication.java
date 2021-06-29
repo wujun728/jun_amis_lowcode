d410fd6766ed47e9ea1508239cb67cd65387435f
wujun728 <wujun728@hotmail.com>
2021-06-30 01:19:02 +0800

    1

--- jun_ssm_boot/jun_boot_generator/src/main/java/com/jun/plugin/code/generator/CodeGenApplication.java
+++ jun_ssm_boot/jun_boot_generator/src/main/java/com/jun/plugin/code/generator/CodeGenApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 启动程序
  * @author cy
  */
-@MapperScan(basePackages = "com.github.wujun728.code.generator.mapper")
+@MapperScan(basePackages = "com.jun.plugin.code.generator.mapper")
 @SpringBootApplication
 public class CodeGenApplication {
     public static void main(String[] args) {
