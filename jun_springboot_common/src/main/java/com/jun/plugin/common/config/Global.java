9510c2f48aec1aa8e2a58a604220e46482469895
wujun728@163.com <wujun728@163.com>
2021-09-28 17:08:35 +0800

    1

--- jun_springboot_common/src/main/java/com/jun/plugin/common/config/Global.java
+++ jun_springboot_common/src/main/java/com/jun/plugin/common/config/Global.java
@@ -87,7 +87,7 @@ public class Global
      */
     public static String getVersion()
     {
-        return StringUtils.nvl(getConfig("royal.version"), "3.0.0");
+        return StringUtils.nvl(getConfig("plugin.version"), "3.0.0");
     }
 
     /**
