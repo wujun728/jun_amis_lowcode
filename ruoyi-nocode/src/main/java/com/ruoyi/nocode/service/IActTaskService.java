a24a84951585fee6f549af4022d6c032ad5ebbe9
wujun <wujun728@163.com>
2024-05-10 10:16:20 +0800

    1

--- ruoyi-nocode/src/main/java/com/ruoyi/nocode/service/IActTaskService.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.nocode.service;
-
-import com.github.pagehelper.Page;
-import com.ruoyi.common.core.page.PageDomain;
-import com.ruoyi.nocode.domain.dto.ActTaskDTO;
-
-public interface IActTaskService {
-    public Page<ActTaskDTO> selectTaskList(PageDomain pageDomain);
-}
--- ruoyi-nocode/src/main/java/com/ruoyi/nocode/service/IActivitiHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.ruoyi.nocode.service;
-
-
-import com.ruoyi.nocode.domain.dto.ActivitiHighLineDTO;
-
-public interface IActivitiHistoryService {
-    public ActivitiHighLineDTO gethighLine(String instanceId);
-}
